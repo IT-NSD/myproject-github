27f086e0f66c930b77d0f25825e250eff6cccd2f
nsd <nsd@scorpio.com>
2023-09-15 10:15:35 +0800

    新增文件，修改提交于2023年9月15日10:15

---
+++ aries/src/com/scorpio/DemoTest.java
@@ -0,0 +1,11 @@
+package com.scorpio;
+
+/**
+ * @author 作者名
+ * @create 2023-09-15 10:11
+ */
+public class DemoTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, China!");
+    }
+}
