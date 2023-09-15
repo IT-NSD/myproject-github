a61ddcc20ad03b461439d1f6922940f493b479ec
nsd <nsd@scorpio.com>
2023-09-15 10:03:09 +0800

    Initial commit

---
+++ aries/src/com/scorpio/Demo.java
@@ -0,0 +1,11 @@
+package com.scorpio;
+
+/**
+ * @author 作者名
+ * @create 2023-09-15 9:58
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
